3359aa2a0171b0df081245662783acf49857161f
nukosuke <nukosuke@shibuya.tech>
2016-09-25 00:24:28 +0900

    add watching subcommand

--- main.go
+++ main.go
@@ -1,71 +1,110 @@
 package main
 
 import (
-  "fmt"
-  "github.com/urfave/cli"
-  "github.com/pascalw/go-alfred"
-  "github.com/spf13/viper"
-  "github.com/nukosuke/go-annict/annict"
-  "golang.org/x/oauth2"
-  "os"
+	"errors"
+	"fmt"
+	"github.com/nukosuke/go-annict/annict"
+	"github.com/pascalw/go-alfred"
+	"github.com/spf13/viper"
+	"github.com/urfave/cli"
+	"golang.org/x/oauth2"
+	"os"
 )
 
 const (
-  bundleID = "tech.shibuya.alfred-annict-workflow"
-  version  = "0.0.0"
+	bundleID = "tech.shibuya.alfred-annict-workflow"
+	version  = "0.0.0"
 )
 
 func newAnnictClient() (*annict.Client, error) {
-  var client *annict.Client
-  return client, nil
+	var client *annict.Client
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(os.Getenv("HOME") + "/.annict")
+	viper.SetConfigType("json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	access_token := viper.GetString("access_token")
+	if access_token == "" {
+		client = annict.NewClient(nil)
+		return client, errors.New("access_token was not found")
+	}
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: access_token})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client = annict.NewClient(tc)
+
+	return client, nil
 }
 
 func main() {
-  app := cli.NewApp()
-  app.Name    = "alfred-annict-workflow"
-  app.Version = version
-  app.Commands = []cli.Command{
-    {
-      Name: "works",
-      Action: func(c *cli.Context) error {
-        viper.SetConfigName("config")
-        viper.AddConfigPath(os.Getenv("HOME")+"/.annict")
-        viper.SetConfigType("json")
-        err := viper.ReadInConfig()
-        if err != nil {
-          return err
-        }
+	app := cli.NewApp()
+	app.Name = "alfred-annict-workflow"
+	app.Version = version
+	app.Commands = []cli.Command{
+		{
+			Name: "works",
+			Action: func(c *cli.Context) error {
+				query := c.Args()
+				client, err := newAnnictClient()
 
-        access_token := viper.GetString("access_token")
+				if err != nil {
+					return err
+				}
 
-        query := c.Args()
+				res, _, err := client.Works.List(&annict.WorksListOptions{
+					FilterTitle: query[0],
+				})
 
-        ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: access_token})
-        tc := oauth2.NewClient(oauth2.NoContext, ts)
-        client := annict.NewClient(tc)
+				if err != nil {
+					return err
+				}
 
-        res, _, err := client.Works.List(&annict.WorksListOptions{
-          FilterTitle: query[0],
-        })
+				list := alfred.NewResponse()
+				for _, item := range res.Works {
+					list.AddItem(&alfred.AlfredResponseItem{
+						Valid:    true,
+						Uid:      fmt.Sprintf("%d", item.Id),
+						Title:    item.Title,
+						Arg:      fmt.Sprintf("https://annict.com/works/%d", item.Id),
+						Subtitle: item.MediaText + " " + item.SeasonNameText,
+					})
+				}
 
-        if err == nil {
-          list := alfred.NewResponse()
-          for _, item := range res.Works {
-            list.AddItem(&alfred.AlfredResponseItem{
-              Valid: true,
-              Uid: fmt.Sprintf("%d", item.Id),
-              Title: item.Title,
-              Arg: fmt.Sprintf("https://annict.com/works/%d", item.Id),
-              Subtitle: item.MediaText+" "+item.SeasonNameText,
-            })
-          }
+				list.Print()
+				return nil
+			},
+		},
+		{
+			Name: "watching",
+			Action: func(c *cli.Context) error {
+				client, err := newAnnictClient()
 
-          list.Print()
-        }
-        return nil
-      },
-    },
-  }
+				res, _, err := client.Me.Works.List(&annict.MeWorksListOptions{FilterStatus: "watching"})
 
-  app.Run(os.Args)
+				if err != nil {
+					return err
+				}
+
+				list := alfred.NewResponse()
+				for _, item := range res.Works {
+					list.AddItem(&alfred.AlfredResponseItem{
+						Valid:    true,
+						Uid:      fmt.Sprintf("%d", item.Id),
+						Title:    item.Title,
+						Arg:      fmt.Sprintf("https://annict.com/works/%d", item.Id),
+						Subtitle: item.MediaText + " " + item.SeasonNameText,
+					})
+				}
+
+				list.Print()
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
